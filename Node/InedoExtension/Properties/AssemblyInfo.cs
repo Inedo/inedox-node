08209a0a564a65bd6bea0ddc25e24812c373ae9f
gdivis <gdivis@inedo.com>
2024-10-24 10:34:00 -0400

    Sdk 3.1 (#2)
    * update sdk to 3.1
    
    * update sdk

--- Node/InedoExtension/Properties/AssemblyInfo.cs
+++ Node/InedoExtension/Properties/AssemblyInfo.cs
@@ -1,14 +1,13 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
-using Inedo.Extensibility;
 
 [assembly: AssemblyTitle("Node")]
 [assembly: AssemblyDescription("Contains operations to integrate with Node & npm.")]
 [assembly: AssemblyCompany("Inedo, LLC")]
 [assembly: AssemblyProduct("BuildMaster")]
-[assembly: AssemblyCopyright("Copyright © Inedo 2023")]
-[assembly: AssemblyVersion("2.2.0")]
-[assembly: AssemblyFileVersion("2.2.0")]
+[assembly: AssemblyCopyright("Copyright © Inedo 2024")]
+[assembly: AssemblyVersion("3.1.0")]
+[assembly: AssemblyFileVersion("3.1.0")]
 [assembly: ComVisible(false)]
 
 [assembly: AppliesTo(InedoProduct.BuildMaster | InedoProduct.Otter)]
